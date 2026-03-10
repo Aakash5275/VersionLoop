26e43430f0e51f066dfd9c800cb6e4c73df18ef6
Aakash5275 <aakashgupta5275@gmail.com>
2026-03-10 08:05:42 +0530

    Router and Controlllers are updated

--- backend/routes/main.router.js
+++ backend/routes/main.router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userRouter = require('./user.router');
 const repoRouter = require('./repo.router');
+const issueRouter = require('./issue.router');
 
 const mainRouter = express.Router();
 
 // 1. Mount the userRouter
 mainRouter.use(userRouter);
 mainRouter.use(repoRouter);
+mainRouter.use(issueRouter);
 
 // 2. FIX: Change 'app.get' to 'mainRouter.get'
 mainRouter.get("/", (req, res) => {
