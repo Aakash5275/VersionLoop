bee4a2afbfe1e6824672640c777be60f04634636
Aakash5275 <aakashgupta5275@gmail.com>
2026-03-10 07:50:48 +0530

    Router and Controlllers are updated

---
+++ backend/controllers/repoController.js
@@ -0,0 +1,49 @@
+const createRepository = (req, res) => {
+    res.send('Repository created');
+};
+
+const getAllRepositories = (req, res) => {
+    res.send('All repositories fetched');
+};
+
+const fetchRepositortById = (req, res) => {
+    res.send('Repository fetched by id');
+};
+
+const fetchRepositoryByName = (req, res) => {
+    res.send('Repository fetched by name');
+};
+
+const fetchRepositoryForCurrentUser = (req, res) => {
+    res.send('Repository for Logged in user fetched');
+};
+
+const updateRepositoryById = (req, res) => {
+    res.send('Repository Updated');
+};
+
+const toggleVisibilityById = (req, res) => {
+    res.send('Repository toggled');
+};
+
+const deleteRepositoryById = (req, res) => {
+    res.send('Repository Deleted');
+};
+
+
+moudle.exports = {
+    createRepository,
+    getAllRepositories,
+    fetchRepositortById,
+    fetchRepositoryByName,
+    fetchRepositoryForCurrentUser,
+    updateRepositoryById,
+    toggleVisibilityById,
+    deleteRepositoryById,
+};
+
+
+
+
+
+
--- backend/routes/main.router.js
+++ backend/routes/main.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRouter = require('./user.router');
+const repoRouter = require('./repo.router');
 
 const mainRouter = express.Router();
 
 // 1. Mount the userRouter
 mainRouter.use(userRouter);
+mainRouter.use(repoRouter);
 
 // 2. FIX: Change 'app.get' to 'mainRouter.get'
 mainRouter.get("/", (req, res) => {
